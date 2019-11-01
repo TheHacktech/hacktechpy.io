65752e98813d95608cb4b06bfb69ef9bb51b4199
Helena Wu <helenawu1998@gmail.com>
2019-11-01 20:56:07 +0000

    Connect applications endpoint to db

---
+++ hacktech/modules/applications/helpers.py
@@ -0,0 +1,63 @@
+import flask
+
+
+def handle_update_applications(email, phone_number, school, major, degree_type, 
+        graduation_year, github, linkedin, resume, latino, race, gender, 
+        shirt_size, need_transportation, bus_from, airport, 
+        dietary_restrictions, diet_choices, diet_details, q1, q2, q3, q4, 
+        code_of_conduct):
+    """Handles application updates by updating the applications table in 
+    the database with application form info."""
+    query = "SELECT user_id FROM users WHERE email = %s"
+    with flask.g.pymysql_db.cursor() as cursor:
+        cursor.execute(query, email)
+        result = cursor.fetchone()
+    user_id = result['user_id']
+    if not user_id:
+        return (False, "Invalid user ID. Please contact the organizers.")
+    # TODO: save function, check for existing application by user id
+    flask.g.pymysql_db.begin()
+    try:
+        # Insert the new row into applications.
+        query = """
+        INSERT INTO applications (user_id, application_year, phone, school, 
+        major, degree_type, graduation_year, github, linkedin, resume, latino,
+        gender, shirt_size, transportation, in_state, bus_from, airport, 
+        diet_rest, diet_rest_detail, q1, q2, q3, q4, code_of_conduct)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 
+        %s, %s, %s, %s, %s, %s, %s, %s, %s )
+        """
+        latino = (latino != "False")
+        in_state = (need_transportation != "outsideCA")
+        transportation = (need_transportation != "no")
+        diet_rest = (dietary_restrictions != "True")
+        code_of_conduct = (code_of_conduct != "False")
+        with flask.g.pymysql_db.cursor() as cursor:
+            cursor.execute(query, [user_id, "2020", phone_number, school, 
+                major, degree_type, graduation_year, github, linkedin,  resume,
+                latino, gender, shirt_size, transportation, in_state,
+                bus_from, airport, diet_rest, diet_details, q1, 
+                q2, q3, q4, code_of_conduct])
+        # Insert new row into diet table for each dietary restriction choice.
+        query = """
+        INSERT INTO diet (user_id, diet_restrictions)
+        VALUES(%s, %s)
+        """
+        for diet in diet_choices:
+            with flask.g.pymysql_db.cursor() as cursor:
+                cursor.execute(query, [user_id, diet])
+        # Insert new row into race table for each race type.
+        query = """
+        INSERT INTO race (user_id, race_type)
+        VALUES(%s, %s)
+        """
+        for race_type in race:
+            with flask.g.pymysql_db.cursor() as cursor:
+                cursor.execute(query, [user_id, race_type])
+        flask.g.pymysql_db.commit()
+    except Exception as e:
+         print(e)
+         flask.g.pymysql_db.rollback()
+         return (False,
+                 "An unexpected error occurred. Please contact the organizers.")
+    return (True, "")
--- hacktech/modules/applications/routes.py
+++ hacktech/modules/applications/routes.py
@@ -1,12 +1,51 @@
 import flask
-
+from hacktech import auth_utils
 from hacktech.modules.applications import blueprint, helpers
 
 @blueprint.route("/applications")
 def applications():
+    if not auth_utils.check_login():
+        return auth_utils.login_redirect()
     # TODO: pre-fill with already existing information!
     return flask.render_template("applications.html")
 
-@blueprint.route("/applications/update")
+@blueprint.route("/applications/update", methods=["POST"])
 def update_applications():
+    """Handles an application update request."""
+    if not auth_utils.check_login():
+        return auth_utils.login_redirect()  
+    email = flask.session['username']
+    phone_number = flask.request.form.get("phoneNumber", None)
+    school = flask.request.form.get("school", None)
+    major = flask.request.form.get("major", None)
+    degree_type = flask.request.form.get("degreeType", None)
+    graduation_year = flask.request.form.get("graduationYear", None)
+    github = flask.request.form.get("github", None)
+    linkedin = flask.request.form.get("linkedin", None)
+    resume = flask.request.form.get("resume", None)
+    latino = flask.request.form.get("latino", None)
+    race = flask.request.form.getlist("race", None)
+    gender = flask.request.form.get("gender", None)
+    shirt_size = flask.request.form.get("shirtSize", None)
+    need_transportation = flask.request.form.get("needTransportation", None)
+    bus_from = flask.request.form.get("busFrom", None)
+    airport = flask.request.form.get("airport", None)
+    dietary_restrictions = flask.request.form.get("dietaryRestrictions", None)
+    diet_choices = flask.request.form.getlist("dietaryRestrictionsChoices", None)
+    diet_details = flask.request.form.get("dietaryRestrictionsDetail", None)
+    q1 = flask.request.form.get("Q1", None)
+    q2 = flask.request.form.get("Q2", None)
+    q3 = flask.request.form.get("Q3", None)
+    q4 = flask.request.form.get("Q4", None)
+    code_of_conduct = flask.request.form.get("codeOfConduct", None)
+
+    success, error_msg = helpers.handle_update_applications(email, phone_number,
+            school, major, degree_type, graduation_year, github, linkedin, resume,
+            latino, race, gender, shirt_size, need_transportation, bus_from, airport,
+            dietary_restrictions, diet_choices, diet_details, q1, q2, q3, q4, code_of_conduct)
+    if success:
+        flask.flash("Your application has been submitted!")
+    else:
+        flask.flash(error_msg)
+        return flask.redirect(flask.url_for("applications.applications"))
     return flask.redirect(flask.url_for("home"))
