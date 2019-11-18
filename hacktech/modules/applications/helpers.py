bb64a78e7395069ff679c54e85d1ff75028b873d
Helena Wu <helenawu1998@gmail.com>
2019-11-18 05:35:12 +0000

    Update application in db on save

--- hacktech/modules/applications/helpers.py
+++ hacktech/modules/applications/helpers.py
@@ -72,10 +72,9 @@ def handle_update_applications(
     user_id = result['user_id']
     if not user_id:
         return (False, "Invalid user ID. Please contact the organizers.")
-    # TODO: save function, check for existing application by user id
     flask.g.pymysql_db.begin()
     try:
-        # Insert the new row into applications.
+        # Insert the new row into applications, or update if it exists.
         query = """
         INSERT INTO applications (user_id, application_year, phone, school, 
         major, degree_type, graduation_year, github, linkedin, resume, latino,
@@ -83,6 +82,29 @@ def handle_update_applications(
         diet_rest, diet_rest_detail, q1, q2, q3, q4, code_of_conduct)
         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 
         %s, %s, %s, %s, %s, %s, %s, %s, %s )
+        ON DUPLICATE KEY UPDATE
+        phone = VALUES(phone),
+        school = VALUES(school),
+        major = VALUES(major),
+        degree_type = VALUES(degree_type),
+        graduation_year = VALUES(graduation_year),
+        github = VALUES(github),
+        linkedin = VALUES(linkedin),
+        resume = VALUES(resume),
+        latino = VALUES(latino),
+        gender = VALUES(gender),
+        shirt_size = VALUES(shirt_size),
+        transportation = VALUES(transportation),
+        in_state = VALUES(in_state),
+        bus_from = VALUES(bus_from),
+        airport = VALUES(airport),
+        diet_rest = VALUES(diet_rest),
+        diet_rest_detail = VALUES(diet_rest_detail),
+        q1 = VALUES(q1),
+        q2 = VALUES(q2),
+        q3 = VALUES(q3),
+        q4 = VALUES(q4),
+        code_of_conduct = VALUES(code_of_conduct)
         """
         latino = (latino != "False")
         in_state = (need_transportation != "outsideCA")
@@ -96,6 +118,13 @@ def handle_update_applications(
                 shirt_size, transportation, in_state, bus_from, airport,
                 diet_rest, diet_details, q1, q2, q3, q4, code_of_conduct
             ])
+        # Delete existing rows in diet table for this user. 
+        query = """
+        DELETE FROM diet
+        WHERE user_id = %s
+        """
+        with flask.g.pymysql_db.cursor() as cursor:
+            cursor.execute(query, [user_id])
         # Insert new row into diet table for each dietary restriction choice.
         query = """
         INSERT INTO diet (user_id, diet_restrictions)
@@ -104,6 +133,13 @@ def handle_update_applications(
         for diet in diet_choices:
             with flask.g.pymysql_db.cursor() as cursor:
                 cursor.execute(query, [user_id, diet])
+        # Delete existing rows in race table for this user. 
+        query = """
+        DELETE FROM race
+        WHERE user_id = %s
+        """
+        with flask.g.pymysql_db.cursor() as cursor:
+            cursor.execute(query, [user_id])
         # Insert new row into race table for each race type.
         query = """
         INSERT INTO race (user_id, race_type)
