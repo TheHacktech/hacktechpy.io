cf6aae64218b13186c61655e2ed599844a2c31ea
Helena Wu <helenawu1998@gmail.com>
2020-02-29 21:18:12 +0000

    Add workshops to schedule

--- hacktech/static/js/schedule.js
+++ hacktech/static/js/schedule.js
@@ -9,9 +9,16 @@ var schedule = [
 // food
 
   title: "Dinner",
-  caption: "",
+  caption: "Come enjoy some Teriyaki rice bowls and meet fellow hackers!",
   time: {day: "fri", start: "6:30PM", duration: "1:30"},
   location: "Bechtel",
+  event_type: "Main timeline"
+},
+{
+  title: "Midnight Snack",
+  caption: "What's poppin, hackers! (Come enjoy fresh popcorn)!",
+  time: {day: "sat", start: "12:30AM", duration: "1:00"},
+  location: "Avery",
   event_type: "Main timeline"
 },
 {
@@ -25,7 +32,7 @@ var schedule = [
 {
   title: "Lunch",
   caption: "Scrumptious barbeque cooked with love by our lovely volunteers",
-  time: {day: "sat", start: "11:00AM", duration: "2:00"},
+  time: {day: "sat", start: "11:30AM", duration: "1:30"},
   location: "Bechtel",
   event_type: "Main timeline"
 },
@@ -56,7 +63,7 @@ var schedule = [
 
 {
   title: "Lunch",
-  caption: "",
+  caption: "Get hyped for project expo while enjoying In-N-Out!",
   time: {day: "sun", start: "11:00AM", duration: "1:00"},
   location: "Outside Avery",
   event_type: "Main timeline",
@@ -65,30 +72,79 @@ var schedule = [
 
 // workshops
 {
-  title: "Sponsor Workshop",
-  caption: "TBA",
-  time: {day: "fri", start: "8:00 PM", duration: "1:00"},
+  title: "[eBay] Innovation & Hacktech-Help Workshop",
+  caption: "Come learn about innovation at eBay and how to get started working with eBay’s APIs. This session will kick-off with a short innovation docuseries which follows teams as they go through the Innovation Program at eBay. Afterwards, we’ll dive into a working session where you will learn about eBay’s public APIs and how to get started. We look forward to seeing you there!",
+  time: {day: "fri", start: "9:00 PM", duration: "1:00"},
   location: "Annenberg 105",
   event_type: "Workshops"
 },
 {
-  title: "Sponsor Workshop",
-  caption: "TBA",
+  title: "[Google Cloud] Three Powerful Google Cloud Products for Your Project",
+  caption: "Come for a chat about Google Cloud computing and Google Cloud Platform! This talk highlights App Engine, Machine Learning APIs, and Cloud Firestore. For each product, there are interactive demos and example use cases. There will be door prizes (aka free shirts!)",
   time: {day: "fri", start: "11:30 PM", duration: "1:00"},
   location: "Annenberg 105",
   event_type: "Workshops"
 },
 {
+  title: "[Esri] Maps and Location Services",
+  caption: "Discover how to use maps and services to change the world. Learn about 2D and 3D technology, geocoding, routing, and other technologies.",
+  time: {day: "sat", start: "1:00 AM", duration: "1:00"},
+  location: "Annenberg 105",
+  event_type: "Workshops"
+},
+{
   title: "Hacking the NASA JPL Way: Solving Space Challenges",
-  caption: "Meet astronaut Larry James!",
+  caption: "Meet astronaut Larry James! He is currently the deputy director of NASA’s Jet Propulsion Laboratory and he will be speaking about his incredible experiences in solving space challenges.",
   time: {day: "sat", start: "8:45 AM", duration: "1:00"},
   location: "Annenberg 105",
   event_type: "Workshops"
 },
 {
+  title: "[Oracle] Build Cloud-Native Applications with Oracle Cloud",
+  caption: "Come and join us to see how you too can build cloud-native applications on Oracle Cloud. We will take you through the journey of MuShop, a fully cloud-native, microservices based online shop. You will discover how to leverage cloud technology to build leading-edge applications for the world of tomorrow.",
+  time: {day: "sat", start: "10:00 AM", duration: "1:00"},
+  location: "Annenberg 105",
+  event_type: "Workshops"
+},
+{
+	title: "[Keystone Strategy] Turning Data into a Story: The Secret Tales of Commit Logs",
+  caption: "In today's world, data is created everywhere - including in your codebase. Good developers track projects with work planning tools and tickets, and that tracking can create a host of information contained within version control software and the code itself. At Keystone Strategy, we are often working to turn data into a story, and commit logs are goldmine. Understanding how to extract information and a project narrative from codebases allows us to understand the environmental factors altering project development and better inform our clients development narrative. Join us as we teach your teams how to analyze your projects to find insights about your working style and the projects development history.",
+  time: {day: "sat", start: "11:00 AM", duration: "0:30"},
+  location: "Annenberg 105",
+  event_type: "Workshops"
+},
+{
   title: "How to Host Your Web App on the Cloud for Free",
-  caption: "Alex Cui",
+  caption: "Caltech student Alex Cui will lead this workshop on hosting a web app publically on the cloud!",
   time: {day: "sat", start: "1:00 PM", duration: "1:00"},
+  location: "Annenberg 105",
+  event_type: "Workshops"
+},
+{
+  title: "Mental Health",
+  caption: "Caltech Professor Adam Blank will speak about mental health.",
+  time: {day: "sat", start: "2:00 PM", duration: "1:00"},
+  location: "Annenberg 105",
+  event_type: "Workshops"
+},
+{
+  title: "[Office Ally] Office Ally Q & A",
+  caption: "Come ask any questions about Office Ally, and meet Chief Technology Officer (and keynote speaker) Jay Wu, and Development Deployment Manager David Liljeblad.",
+  time: {day: "sat", start: "3:00 PM", duration: "0:30"},
+  location: "Annenberg 105",
+  event_type: "Workshops"
+},
+{
+  title: "[Keystone Strategy] The AI Impact: How Technology is Changing Business Decisions",
+  caption: "It should come as no surprise that artificial intelligence and machine learning are reshaping the business landscape, but where do these technical investments create the most impact? Join us as we review the current and potential value these technologies create across industries, based on the latest work from Harvard Business School Digital Innovation Chair Marco Iansiti. We will take a deeper dive into how these technologies are driving business decision making, and discuss how you can leverage this to create maximum impact via your hacks. ",
+  time: {day: "sat", start: "5:00 PM", duration: "0:30"},
+  location: "Annenberg 105",
+  event_type: "Workshops"
+},
+{
+  title: "SpatialDB: Enabling Rigorous Geospatial Queries",
+  caption: "Come hear Caltech student Rupesh Jeyaram speak about his research project on building spatial databases to access, explore, and navigate the terabytes of data. ",
+  time: {day: "sat", start: "5:30 PM", duration: "0:45"},
   location: "Annenberg 105",
   event_type: "Workshops"
 },
@@ -171,7 +227,7 @@ var schedule = [
 {
   title: "Snack Mix & Mingle",
   caption: "Come grab some snacks and mingle with other hackers!",
-  time: {day: "fri", start: "9:00PM", duration: "1:00"},
+  time: {day: "fri", start: "8:00PM", duration: "1:00"},
   location: "Avery",
   event_type: "events"
 },
@@ -207,6 +263,13 @@ var schedule = [
   event_type: "events"
 },
 
+{
+  title: "Capture the Flag",
+  caption: "Hosted by MLH.",
+  time: {day:"sat", start:"7:00PM", duration:"1:00"},
+  location: "Avery Conference",
+  event_type: "events"
+},
 {
   title: "Werewolf",
   caption: "Find the werewolves among yourselves! Hosted by MLH.",
