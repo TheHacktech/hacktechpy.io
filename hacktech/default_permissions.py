a9ba3363eb22e36a8dfad8b2dd6b3566ca8caddd
Eugene Shao <eshao573@gmail.com>
2022-01-21 23:27:37 -0800

    help

--- hacktech/default_permissions.py
+++ hacktech/default_permissions.py
@@ -6,21 +6,6 @@ For module specific permissions, create an Enum within the module
 '''
 
 import enum
-from hacktech import environment
-
-# Test Database
-TEST = environment.Environment(
-    db_hostname="localhost",
-    db_name="hacktech_test",
-    db_user="hacktech_test",
-    db_password="",
-    debug=False,
-    testing=False,
-    secret_key="111",
-    em="12334",
-    email="122222"
-)
-
 
 class Permissions(enum.IntEnum):
     #Site admins -- always have permission to everything -- Use with caution
